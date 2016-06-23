e54b6aa72df7967e7eb775808171c09e56703f8b
Timothy Woo <timwoo24@gmail.com>
2016-06-23 15:21:23 -0700

    add keys

---
+++ server/apiKeys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumerKey: 'M4AdD43rFrwW8bBM84G3cw',
+  consumerSecret: 'wXcftu8TTwx_2BNw_NHBvEMxyuI',
+  token: 'hqvoL8r8cXhDgU6XIFyao2lcLEUjmCwK',
+  tokenSecret: 'kvOM6FdaMmIlMb86qWpdUXTl9Vc'
+}
